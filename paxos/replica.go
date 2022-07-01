84da4fe71ea934a62252603d7bad94c451ab31ff
sofiaregis <79062855+sofiaregis@users.noreply.github.com>
2022-07-01 14:51:07 -0300

    Update replica.go

--- paxos/replica.go
+++ paxos/replica.go
@@ -25,20 +25,6 @@ type Replica struct {
 	*Paxos
 }
 
-// NewReplica generates new Paxos replica
-func NewReplica(id paxi.ID) *Replica {
-	r := new(Replica)
-	r.Node = paxi.NewNode(id)
-	r.Paxos = NewPaxos(r)
-	r.Register(paxi.Request{}, r.handleRequest)
-	r.Register(P1a{}, r.HandleP1a)
-	r.Register(P1b{}, r.HandleP1b)
-	r.Register(P2a{}, r.HandleP2a)
-	r.Register(P2b{}, r.HandleP2b)
-	r.Register(P3{}, r.HandleP3)
-	return r
-}
-
 func (r *Replica) handleRequest(m paxi.Request) {
 	log.Debugf("Replica %s received %v\n", r.ID(), m)
 
